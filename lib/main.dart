8342f956ceba43026826bd16b5687f0fb5c9ecd6
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-02-18 16:44:17 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+        home: Scaffold(
       appBar: AppBar(title: Text("MyApp"), backgroundColor: Colors.red),
       body: ListView(children: <Widget>[
-          Container(
+        Container(
             padding: EdgeInsets.all(16.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -32,118 +32,114 @@ class MyApp extends StatelessWidget {
                 ]),
               ],
             )),
+        Container(
+          padding: EdgeInsets.all(2.0),
+          color: Colors.purpleAccent,
+          child: Column(children: <Widget>[
+            Image.network("https://pbs.twimg.com/media/EuLdHcZVEAEIGMD.jpg"),
 
-          Container(
-            padding: EdgeInsets.all(2.0),
-            color: Colors.purpleAccent,
-            child: Column(
-              children: <Widget>[
-                Image.network(
-                "https://pbs.twimg.com/media/EuLdHcZVEAEIGMD.jpg"),
+            //Text("Costa Mendekat Ke Palmeiras",
+            //style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 10))
+            Container(
+              padding: EdgeInsets.all(8.0),
+              color: Colors.white,
+              alignment: Alignment.center,
+              child: Center(
+                child: Text("Costa Mendekat Ke Palmeiras",
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              ),
+            ),
 
-                //Text("Costa Mendekat Ke Palmeiras",
-                //style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 10))
-                Container(
-                  padding: EdgeInsets.all(8.0),
-                  color: Colors.white, 
-                  alignment:Alignment.center, 
-                  child: Center(
-                    child: Text("Costa Mendekat Ke Palmeiras", 
-                    style: TextStyle(fontSize:18, fontWeight: FontWeight.bold)),
-                  ), 
-                ),
-
-                Container(
-                  padding: EdgeInsets.all(5.0),
-                  color: Colors.purpleAccent, 
-                  alignment:Alignment.centerLeft, 
-                  child: Text("Transfer", 
-                  style: TextStyle(fontSize:12,color: Colors.black)), 
-                  height: 50.0, 
-                  width: 600.0,
-                ),
-              ]),
+            Container(
+              padding: EdgeInsets.all(5.0),
+              color: Colors.purpleAccent,
+              alignment: Alignment.centerLeft,
+              child: Text("Transfer",
+                  style: TextStyle(fontSize: 12, color: Colors.black)),
+              height: 50.0,
+              width: 600.0,
+            ),
+          ]),
         ),
-
-        Container( 
-          decoration: BoxDecoration(border: Border.all(color: Colors.black)), 
-          margin: EdgeInsets.only(top: 10), 
-            child: Row(
-              children: <Widget>[
-                Column(children: <Widget>[
-                  
-                  Image.network("https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800",
+        Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          margin: EdgeInsets.only(top: 10),
+          child: Row(
+            children: <Widget>[
+              Column(children: <Widget>[
+                Image.network(
+                  "https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800",
                   width: 150.0,
                   height: 120.0,
-                  fit: BoxFit.cover,),
-                
-                ]),
-                
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  alignment: Alignment.center,
-                  child: Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tapuk Jidat", 
-                  maxLines: 2,
-                  style: TextStyle(color: Colors.black, fontSize: 12)),
-                  height: 120,
-                  width: 170,
+                  fit: BoxFit.cover,
                 ),
+              ]),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                alignment: Alignment.center,
+                child: Text(
+                    "Pique Bilang Wasit Untungkan Madrid, Koeman Tapuk Jidat",
+                    maxLines: 2,
+                    style: TextStyle(color: Colors.black, fontSize: 12)),
+                height: 120,
+                width: 170,
+              ),
             ],
           ),
         ),
-
         Container(
           child: Row(
             children: <Widget>[
               Container(
                 padding: EdgeInsets.all(5.0),
-                decoration: BoxDecoration( border: Border.all(color: Colors.black)),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
                 alignment: Alignment.centerLeft,
                 child: Text("Barcelona Feb 13, 2021",
-                            style: TextStyle(color: Colors.black)),
+                    style: TextStyle(color: Colors.black)),
                 height: 30,
                 width: 360,
               ),
             ],
           ),
         ),
-
-        Container( 
-          decoration: BoxDecoration(border: Border.all(color: Colors.black)), 
-          margin: EdgeInsets.only(top: 10), 
-            child: Row(
-              children: <Widget>[
-                Column(children: <Widget>[
-                  
-                  Image.network("https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800",
+        Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          margin: EdgeInsets.only(top: 10),
+          child: Row(
+            children: <Widget>[
+              Column(children: <Widget>[
+                Image.network(
+                  "https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800",
                   width: 150.0,
                   height: 120.0,
-                  fit: BoxFit.cover,),
-                
-                ]),
-                
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  alignment: Alignment.center,
-                  child: Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tapuk Jidat", 
-                  maxLines: 2,
-                  style: TextStyle(color: Colors.black, fontSize: 12)),
-                  height: 120,
-                  width: 170,
+                  fit: BoxFit.cover,
                 ),
+              ]),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                alignment: Alignment.center,
+                child: Text(
+                    "Pique Bilang Wasit Untungkan Madrid, Koeman Tapuk Jidat",
+                    maxLines: 2,
+                    style: TextStyle(color: Colors.black, fontSize: 12)),
+                height: 120,
+                width: 170,
+              ),
             ],
           ),
         ),
-
         Container(
           child: Row(
             children: <Widget>[
               Container(
                 padding: EdgeInsets.all(5.0),
-                decoration: BoxDecoration( border: Border.all(color: Colors.black)),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
                 alignment: Alignment.centerLeft,
                 child: Text("Barcelona Feb 13, 2021",
-                            style: TextStyle(color: Colors.black)),
+                    style: TextStyle(color: Colors.black)),
                 height: 30,
                 width: 360,
               ),
